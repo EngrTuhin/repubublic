fd6c07ff71c1ce505423b8cfcce025e06e0ac0fe
EngrTuhin <akramhossaintuhin1@gmail.com>
2026-08-29 12:13:09 +0600

    u

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   reactCompiler: true,
   allowedDevOrigins: ['192.168.0.24', 'localhost:3000', '192.168.0.24:3000','https://repubublic.vercel.app'],
 };
